8f704c0f26873a319717babf5c1e595108d6ce5a
BronyaInUSTC <3218275879@qq.com>
2023-01-16 23:22:35 +0800

    格式化了该文件

--- crawler_main.py
+++ crawler_main.py
@@ -15,23 +15,28 @@ from namer import Namer
 from source_crawler import Crawler
 from hrefs2csv import Extractor
 
+
 def main():
     hc = Href_Collecter()
     namer = Namer()
+
     # 网络检查
     lib.net_check()
+
     # 爬取封面
     driver = Driver(driver_path=driver_path, extension_path=ex_path).blank_driver()
     crawler = Crawler(driver)
     crawler.cover()
     crawler.market()
     crawler.quit()
+
     # 分条爬取
     driver = Driver(extension_path=lib.ex_path).blank_driver()
     ex = Extractor(driver)
     ex.cover(href_list=hc.lead_pos_href_list(namer.cover_name()))
     ex.market(href_list=hc.lead_pos_href_list(namer.market_name()))
     ex.quit()
+
 
 if __name__ == '__main__':
     main()
